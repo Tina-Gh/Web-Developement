ee60bd190f063b7b7beb5ea676090f246aa111c1
Tina Gholami <44522127+Tina-Gh@users.noreply.github.com>
2021-11-07 14:13:25 -0500

    Add files via upload

---
+++ Section 12:/Dice Game/index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+var randomImageSource1 = "images/" + randomDiceImage1;
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1) //The left image tag
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/" + randomDiceImage2;
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2) //The right image tag
+
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 wins!";
+}
+else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "🚩Player 2 wins!";
+} 
+else {
+    document.querySelector("h1").innerHTML = "🚩Draw!";
+}
